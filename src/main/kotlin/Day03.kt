57b8fa91e2a18772b18c13b016baad77a36d26ef
Gideon <gideon_hoog@hotmail.com>
2021-12-03 09:54:20 +0100

    solution day 3 (don't mind the duplication xD)

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -4,18 +4,69 @@ fun main() {
 
 class ProblemDay03 : Problem<Int>("Day03") {
     override fun validatePart1TestInput(result: Int) {
-        check(result == 1)
+        check(result == 198)
     }
 
     override fun validatePart2TestInput(result: Int) {
-        check(result == 1)
+        check(result == 230)
     }
 
     override fun part1(input: List<String>): Int {
-        return 0
+        val gamma = input.toMostCommonBits()
+        val epsilon = gamma.invert()
+        return gamma.toInt(2) * epsilon.toInt(2)
     }
 
     override fun part2(input: List<String>): Int {
-        return 0
+        return input.oxygen().toInt(2) * input.co2().toInt(2)
     }
+
+    private fun List<String>.oxygen():String {
+        var idx = 0
+        var remaining:List<String> = this
+        while(idx < remaining.first().length && remaining.size > 1) {
+            val oxygenBit = remaining.mostCommonBit(idx,-1)
+            remaining = remaining.filter { it[idx] == oxygenBit }
+            idx++
+        }
+        return remaining.first()
+    }
+
+    private fun List<String>.co2():String {
+        var idx = 0
+        var remaining:List<String> = this
+        while(idx < remaining.first().length && remaining.size > 1) {
+            val co2Bit = remaining.mostCommonBit(idx,-1).invert()
+            remaining = remaining.filter { it[idx] == co2Bit }
+            idx++
+        }
+        return remaining.first()
+    }
+
+    private fun String.invert(): String {
+        return this.map { if(it == '1') 0 else 1 }.joinToString(separator = "")
+    }
+
+    private fun List<String>.toMostCommonBits(offset: Int = 0): String {
+        return (0 until this[0].length).map { this.mostCommonBit(it, offset) }.joinToString(separator = "")
+    }
+
+    private fun Char.invert():Char = when(this) {
+        '1' -> '0'
+        '0' -> '1'
+        else -> error("Unknown binary bit")
+    }
+
+    private fun List<String>.mostCommonBit(pos: Int, offset:Int = 0): Char {
+        return if (countOnes(pos) > (this.size + offset) / 2)
+            '1'
+        else '0'
+    }
+
+
+    private fun List<String>.countOnes(pos: Int): Int {
+        return this.count { it.isOne(pos) }
+    }
+
+    private fun String.isOne(pos:Int) = this[pos] == '1'
 }

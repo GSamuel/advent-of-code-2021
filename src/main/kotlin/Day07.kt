b2c7c1f9e60d9481c884a3b10337e77f40542787
Gideon <gideon_hoog@hotmail.com>
2021-12-07 12:28:20 +0100

    solution day 7 part 1 & part 2

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,21 +1,49 @@
+import kotlin.math.abs
+
 fun main() {
     ProblemDay07().solve()
 }
 
 class ProblemDay07 : Problem<Int>("Day07") {
     override fun validatePart1TestInput(result: Int) {
-        check(result == -1)
+        check(result == 37)
     }
 
     override fun validatePart2TestInput(result: Int) {
-        check(result == -1)
+        check(result == 168)
     }
 
     override fun part1(input: List<String>): Int {
-        return 0
+        return input.toCrabs().lowestFuel(LinearCost)
     }
 
     override fun part2(input: List<String>): Int {
-        return 0
+        return input.toCrabs().lowestFuel(NonLinearCost)
     }
+
+    private fun List<String>.toCrabs() = first().splitToInts()
+
+    private fun List<Int>.lowestFuel(cost:CostFunction) = cost.cost(this, findOptimalPosition(cost))
+
+    private fun List<Int>.findOptimalPosition(cost:CostFunction): Int = (0 until this.maxOf { it }).minByOrNull { cost.cost(this, it) }!!
+
+    private fun String.splitToInts() = split(",").toInts()
+
+    sealed class CostFunction {
+        abstract fun cost(value:Int, target:Int): Int
+        fun cost(positions:List<Int>, target:Int):Int = positions.sumOf { cost(it, target) }
+    }
+
+    object LinearCost:CostFunction() {
+        override fun cost(value: Int, target: Int): Int = abs(value - target)
+    }
+
+    object NonLinearCost:CostFunction() {
+        override fun cost(value: Int, target: Int): Int = cost(abs(value - target))
+
+        private fun cost(n: Int): Int {
+            return n * (n+1) /2
+        }
+    }
+
 }
